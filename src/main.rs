334d6afa5729ef512106b8cbdc6d0609003e8587
Alex Sirenko <sirenko.alexey@gmail.com>
2025-11-24 18:36:13 +0200

    Raise error on unknown command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use io::stdin;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = "".to_string();
+    stdin().read_line(&mut command).unwrap();
+    println!("{}: command ot found", command.trim());
 }
