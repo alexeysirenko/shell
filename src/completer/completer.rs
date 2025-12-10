98e50912e22b5ad68191d3fac5ebe8a8a0beb61e
Alex Sirenko <sirenko.alexey@gmail.com>
2025-12-10 17:17:31 +0200

    Fix the test

--- src/completer/completer.rs
+++ src/completer/completer.rs
@@ -35,7 +35,7 @@ impl Completer for ShellCompleter {
             .filter(|cmd| cmd.starts_with(word))
             .map(|cmd| Pair {
                 display: cmd.clone(),
-                replacement: cmd.clone(),
+                replacement: format!("{} ", cmd),
             })
             .collect();
 
