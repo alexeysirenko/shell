5c1eda52440d3b3cf1f306b287a86b7a81c893d6
Alex Sirenko <sirenko.alexey@gmail.com>
2025-12-11 11:21:18 +0200

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use std::collections::HashSet;
 
 use codecrafters_shell::completer::ShellCompleter;
 use codecrafters_shell::finder::ExecutablesFinder;
+use codecrafters_shell::parser::{parse_command, parse_prompt};
 use rustyline::error::ReadlineError;
 use rustyline::{CompletionType, Config, Editor};
 
-use codecrafters_shell::{builtin_commands, handle_command, parse_command, parse_prompt};
+use codecrafters_shell::{builtin_commands, handle_command};
 
 fn main() {
     let path_executables = ExecutablesFinder::new().find_executables_in_path().unwrap();
